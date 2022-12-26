789874c4e70fab363c78de54571df6a07c6e9ef0
Trijun <trijun.mj@gmail.com>
2022-12-26 12:55:19 +0530

    add to cart options completed

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -14,7 +14,28 @@
                     </div>
                   
                     <p>{{$products->description}}</p>
-                    <button class="hero-button">ADD TO CART</button>
+                    <div class="w-100">
+                        <form action="">
+                            <div class="cartForm">
+                                <div class="w-60 flex-column me-4">
+                                    <label for="size"><strong>Size</strong></label>
+                                    <select name="size" class="form-select form-select-lg mt-3" id="size">
+                                        <option value="XS">S</option>
+                                        <option value="XS">M</option>
+                                        <option value="XS">L</option>
+                                    </select>
+                                </div>
+                                <div class="w-40 flex-column ">
+                                    <label for="color"><strong>Color</strong></label>
+                                    <select name="color" class=" form-select form-select-lg mt-3" id="color">
+                                        <option value="black">black</option>
+                                        <option value="white">white</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <input type="submit" class="hero-button mt-4" value="ADD TO CART">
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
